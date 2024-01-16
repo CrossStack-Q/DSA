15eb855b1407b7b867c41a3ca718ba5ef3c26a4a
CrossStack-Q <crossstack@gmail.com>
2024-01-16 20:53:56 +0530

    4th code done

---
+++ removeDuplicates.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> arr={1,1,1,2,2,2,3,3,4,4};
+
+    int i=0;
+    for(int j=1;j<10;j++){
+        if(arr[j]!=arr[i]){
+            arr[i+1]=arr[j];
+            i++;
+        }
+    }
+
+    for(auto it:arr){
+        cout<<it<<endl;
+    }
+}
