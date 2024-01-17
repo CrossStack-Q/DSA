ee18270b7933342c9fadca5dba012229754bf2c2
CrossStack-Q <crossstack@gmail.com>
2024-01-17 13:27:17 +0530

    9th code done

--- moveZeroes.cpp
+++ moveZeroes.cpp
@@ -2,11 +2,19 @@
 using namespace std;
 
 int main(){
-    int arr[8]={1,4,0,0,2,3,0,7};
+    int arr[8]={1,4,2,0,0,0,2,3};
     int j=0;
-    while(arr[j]!=0){
-        j++;
+
+    
+
+
+    for(j;j<8;j++){
+        if(arr[j]==0){
+            break;
+        }
     }
+
+    
 
     for(int i=j+1;i<8;i++){
         if(arr[i]!=0){
